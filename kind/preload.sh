f1076532fabc5551cd251bc4c913e4dc1e2d995d
Roy Alon <42257624+ouzel-alon@users.noreply.github.com>
2023-05-28 20:57:47 +1000

    Preload metallb images

--- kind/preload.sh
+++ kind/preload.sh
@@ -8,6 +8,8 @@ set -u
 set -o pipefail
 
 CALICO_VER="3.26.0"
+METALLB_VER="0.13.9"
+
 IMAGES=(
     "docker.io/calico/apiserver:v$CALICO_VER"
     "docker.io/calico/cni:v$CALICO_VER"
@@ -15,7 +17,8 @@ IMAGES=(
     "docker.io/calico/node-driver-registrar:v$CALICO_VER"
     "docker.io/calico/node:v$CALICO_VER"
     "docker.io/calico/pod2daemon-flexvol:v$CALICO_VER"
-    "quay.io/metallb/controller:v0.9.8"
+    "quay.io/metallb/controller:v$METALLB_VER"
+    "quay.io/metallb/speaker:v$METALLB_VER"
 )
 
 # KUBE_VER="1.26.3"
@@ -40,29 +43,36 @@ fi
 
 echo -e "* Check control node if cluster is running"
 if [[ $(docker inspect -f '{{.State.Running}}' kind-control-plane) ]]; then
-    echo -e "* Download images\n"
-    for i in "${IMAGES[@]}"; do
-        docker image pull "$i";
-    done
-
-    echo -e "* Load image to cluster\n"
-    for i in "${IMAGES[@]}"; do
-        case $CLUSTER_BIN in
-            "kind")
-                kind load docker-image "$i" --name kind;
-                ;;
-            "minikube")
-                minikube image load "$i" --daemon;
-                ;;
-            *)
-                echo "! Unsupported cluster";
-                ;;
-        esac
-    done
-
-    echo -e "* Current images in cluster\n"
-    docker exec kind-control-plane crictl images
+    echo "* Control plane node found"
 else
-    echo "! Can't find the control node. Is the cluster running?"
+    echo "! Can't find a control plane node. Is the cluster running?"
     exit 1
 fi
+
+case $1 in
+    "pull")
+        echo -e "* Download images\n"
+        for i in "${IMAGES[@]}"; do
+            docker image pull "$i";
+        done
+        ;&
+    *)
+        echo -e "* Load image to cluster\n"
+        for i in "${IMAGES[@]}"; do
+            case $CLUSTER_BIN in
+                "kind")
+                    kind load docker-image "$i" --name kind;
+                    ;;
+                "minikube")
+                    minikube image load "$i" --daemon;
+                    ;;
+                *)
+                    echo "! Unsupported cluster";
+                    ;;
+            esac
+        done
+    ;;
+esac
+
+echo -e "* Current images in cluster\n"
+docker exec kind-control-plane crictl images
